789c864309ccb01fde95976dd22d6fe4b6ef364f
Swaroop <swarooprajg@gmail.com>
2016-03-25 12:39:12 -0400

    Initial Check-in

---
+++ com/poorjar/swagger/UserController.java
@@ -0,0 +1,53 @@
+package com.poorjar.swagger;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poorjar.swagger.dataaccess.User;
+import com.poorjar.swagger.dataaccess.UserRepository;
+
+@RestController
+@EnableAutoConfiguration
+public class UserController {
+    private static Logger LOGGER = Logger.getLogger(UserController.class);
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping("/users/all")
+    public List<User> getUsers() {
+        return this.userRepository.findAll();
+    }
+
+    /**
+     * Creates users with the provided info.
+     *
+     * @param users The users to be created.
+     * @return Collection of newly created users.
+     */
+    @RequestMapping(value = "/users/create", method = RequestMethod.POST)
+    public Collection<User> createUsers(@RequestBody Collection<User> users) {
+        LOGGER.error(users);
+        return this.userRepository.save(users);
+    }
+    
+
+    /**
+     * Deletes a specific user.
+     *
+     * @param userId The id of the user to be deleted.
+     */
+    @RequestMapping("/users/delete")
+    public void deleteUser(@RequestBody long userId) {
+        LOGGER.error(userId);
+        return this.userRepository.delete(userId);
+    }
+
+}
---
+++ com/poorjar/swagger/dataaccess/User.java
@@ -0,0 +1,60 @@
+package com.poorjar.swagger.dataaccess;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user", uniqueConstraints = {@UniqueConstraint(columnNames = {"firstname", "lastname"})})
+public class User implements Serializable {
+    private static final long serialVersionUID = -5215603105951570953L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    protected User() {
+    }
+
+    public User(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    /**
+     * @return the id
+     */
+    public final long getId() {
+        return id;
+    }
+
+    /**
+     * @return the firstname
+     */
+    public final String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * @return the lastname
+     */
+    public final String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * @return the email
+     */
+    public final String getEmail() {
+        return email;
+    }
+
+    @SuppressWarnings("boxing")
+    @Override
+    public String toString() {
+        return String.format("User[id=%d, firstName='%s', lastname='%s', email='%s']", id, firstname, lastname, email);
+    }
+}
